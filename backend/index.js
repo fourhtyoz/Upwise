f8404c673bb624ef40f1bde829e0191a8408ec71
fourhtyoz <fourhtyoz@gmail.com>
2025-11-22 01:29:01 +0300

    setup gitignore

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+const homePageHandler = (req, res) => {
     res.status(200).json({message: "test"})
-})
+}
+
+app.get('/', homePageHandler)
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
