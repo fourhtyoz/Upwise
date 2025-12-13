3f7d4a319fe98a9af21800e1b6eeadb89b93fc26
fourhtyoz <fourhtyoz@gmail.com>
2025-12-13 22:58:55 +0300

    prisma dependencies

--- components/common/SocialMediaCircle.tsx
+++ components/common/SocialMediaCircle.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SocialMediaCircle() {
-    return <div>SocialMediaCircle</div>;
+  return <div>SocialMediaCircle</div>;
 }
 
 export default SocialMediaCircle;
