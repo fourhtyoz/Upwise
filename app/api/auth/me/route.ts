47c440f965832e9ec646d3657c71b30b955260c8
fourhtyoz <fourhtyoz@gmail.com>
2025-12-13 23:55:30 +0300

    refactored auth endpoints

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,4 +1,4 @@
-import { getCurrentUser } from "@/app/lib/auth";
+import { getCurrentUser } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,76 +1,68 @@
-import { generateToken, hashPassword } from "@/app/lib/auth";
-import { prisma } from "@/app/lib/db";
+import { generateToken, hashPassword } from "@/lib/auth";
+import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import { Role } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
   try {
-    const { name, email, password, teamCode } = await request.json();
-    if (!name || !email || !password) {
+    const { username, email, firstName, lastName, password } =
+      await request.json();
+    if (!username || !email || !password) {
       return NextResponse.json(
-        { error: "Name, email & password are required or not valid" },
+        { error: "Username, email & password are required or not valid" },
         { status: 400 },
       );
     }
 
-    const existingUser = await prisma.user.findUnique({ where: { email } });
-    if (existingUser) {
+    const emailExists = await prisma.user.findUnique({ where: { email } });
+    if (emailExists) {
       return NextResponse.json(
         { error: "User with this email already exists}" },
         { status: 409 },
       );
     }
 
-    let teamId: string | undefined;
-    if (teamCode) {
-      const team = await prisma.team.findUnique({ where: { code: teamCode } });
-      if (!team) {
-        return NextResponse.json(
-          { error: "Team Code is invalid" },
-          { status: 400 },
-        );
-      }
-      teamId = team.id;
+    const usernameExists = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (usernameExists) {
+      return NextResponse.json(
+        { error: "User with this username already exists}" },
+        { status: 409 },
+      );
     }
 
     const hashedPassword = await hashPassword(password);
 
-    // first user become admin, others become users
-    const userCount = await prisma.user.count();
-    const role = userCount === 0 ? Role.ADMIN : Role.USER;
     const user = await prisma.user.create({
       data: {
-        name,
+        username,
         email,
+        firstName,
+        lastName,
         password: hashedPassword,
-        role,
-        teamId,
-      },
-      include: {
-        team: true,
       },
     });
 
-    const token = generateToken(user.id);
     const response = NextResponse.json({
       user: {
         id: user.id,
         email: user.email,
-        name: user.name,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
         role: user.role,
-        teamId: user.teamId,
-        team: user.team,
-        token,
       },
     });
 
     // set cookie
+    const token = generateToken(user.id);
     response.cookies.set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 60 * 60 * 24 * 7, // days
     });
+
     return response;
   } catch (e) {
     console.error("Registration failed:", e);
